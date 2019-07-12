5baca36766240d40c9642595c9c4aaee177354e2
galejo09 <50342003+galejo09@users.noreply.github.com>
2019-07-12 10:41:22 -0700

    Make suffix a default arg of saveas func

--- FEDCODE/Dual_Imaging/librain.py
+++ FEDCODE/Dual_Imaging/librain.py
@@ -254,7 +254,7 @@ class Output:
         self.directory = directory
 
 
-    def saveas(self, f_out, suffix, ftype, prefix=None, dtype=None, f_in=None, save=False, path=None, dirname="Derivatives", fig=False):
+    def saveas(self, f_out, ftype, suffix=None, prefix=None, dtype=None, f_in=None, save=False, path=None, dirname="Derivatives", fig=False):
         """
         Returns file name of a result with the same naming convention as the corresponding raw data. 
         The result can also be saved.
