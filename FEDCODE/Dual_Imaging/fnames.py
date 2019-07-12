9008a47097823b30b293c847d51871589bdd3f61
galejo09 <50342003+galejo09@users.noreply.github.com>
2019-07-12 11:01:10 -0700

    Remove mean and fixed stim frames; add RM mask

--- FEDCODE/Dual_Imaging/fnames.py
+++ FEDCODE/Dual_Imaging/fnames.py
@@ -9,11 +9,10 @@ fnames = {  ###Behaviour###
     "interpolated": "Behaviour/M802L_M802R_February-21_1312_experiment-1_wt_cage-mates_mouse_rail_M002Gl_interpolated.raw",
     "h264": "M902L_M903R_March-16_1444_experiment-1_wt_cage-mates_together_12mm.h264",
     ###No subfolder###
-    "combined": "",
-    "processed": "",
-    "mean_stim_frames": "",
-    "fixed_stim_frames": "",
-    "LM_mask": "",
+    "combined": "combined_interp_dff0_bandpass0.01-3.0Hz_.raw",
+    "processed": "processed_MM802L_February-21_1313_experiment-1_M802R_wt_cage-mates_mouse_rail_M002Gl_interp_dff0_bandpass0.01-3.0Hz_.raw",
+    "RM mask": "RM_mask.npy",
+    "LM mask": "LM_mask.npy",
     "left blue": "LEFT_RAW_BLUE_FRAME.npy",
     "left green": "LEFT_RAW_GREEN_FRAME.npy",
     "right blue": "RIGHT_RAW_BLUE_FRAME.npy",
@@ -34,10 +33,10 @@ fnames = {  ###Behaviour###
     "right": "MM902R_April-20_1329_experiment-3_M804L_wt_non-cage-mates_first_meet_together_12mm.raw",
     "left timestamps": "MM804L_April-20_1332_experiment-3_M902R_wt_non-cage-mates_first_meet_together_12mm_timestamps.raw",
     "right timestamps": "MM902R_April-20_1329_experiment-3_M804L_wt_non-cage-mates_first_meet_together_12mm_timestamps.raw",
-    "freq split ws=3800" : "frequency_split_correlation_filtered_ws=3800.py.npy",
-    "freq split ws=5000" : "frequency_split_correlation_filtered_ws=5000.py.npy",
-    "freq split ws=1000" : "frequency_split_correlation_filtered_ws=1000.py.npy",
-    "freq split ws=2000" : "frequency_split_correlation_filtered_ws=2000.py.npy",
-    "freq split ws=2800" : "frequency_split_correlation_filtered_ws=2800.py.npy",
-    "trunc" : "LEFT_GREEN_processed__interp_dff0_bandpass0.01-3.0Hz_MM803L_March-12_1520_experiment-2_M804R_wt_cage-mates_mouse_rail_together_12mm_TRUNCATED.npy"
+    "freq split ws=3800": "frequency_split_correlation_filtered_ws=3800.py.npy",
+    "freq split ws=5000": "frequency_split_correlation_filtered_ws=5000.py.npy",
+    "freq split ws=1000": "frequency_split_correlation_filtered_ws=1000.py.npy",
+    "freq split ws=2000": "frequency_split_correlation_filtered_ws=2000.py.npy",
+    "freq split ws=2800": "frequency_split_correlation_filtered_ws=2800.py.npy",
+    "trunc": "LEFT_GREEN_processed__interp_dff0_bandpass0.01-3.0Hz_MM803L_March-12_1520_experiment-2_M804R_wt_cage-mates_mouse_rail_together_12mm_TRUNCATED.npy"
 }
