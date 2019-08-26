e210ebfb4d36be82a37cdad7fcd82f0a00f2aa99
Ashutosh Bhudia <ashu.bhudia@gmail.com>
2019-08-26 10:56:43 -0700

    add script

---
+++ FEDCODE/Dual_Imaging/make_readme.py
@@ -0,0 +1,13 @@
+import os
+
+with open('README.md', 'w') as wf:
+    wf.write(
+        """
+# Static Renderings of These Notebooks
+___
+\n"""
+    )
+    for f in os.listdir():
+        if 'ipynb' in f and 'checkpoints' not in f:
+            wf.write(
+                "## [{}](https://github/ubcbraincircuits/Dual_Imaging/blob/master/FEDCODE/Dual_Imaging/{})\n".format(f, f.replace(" ", "%20"))) 
